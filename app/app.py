678b2cacfc8b4762b1bd6d146cd9621186e6a9aa
liwentao <liwentao@douban.com>
2013-10-29 21:12:09 +0800

    test session

--- app/app.py
+++ app/app.py
@@ -10,25 +10,39 @@ STATIC_ROOT = os.path.join(APP_ROOT, 'static')
 sys.path.insert(0, LIBS_ROOT)
 
 import bottle
-from bottle import Bottle, static_file
+from bottle import Bottle, static_file, request
 from bottle import mako_template as template
+from beaker.middleware import SessionMiddleware
 
 bottle.TEMPLATE_PATH.insert(0, TEMPLATES_ROOT)
-
+session_options = {
+    'session.type': 'cookie',
+    'session.validate_key': True,
+}
 
 app = Bottle()
 
 
 @app.route('/')
-@app.route('/<name>')
+@app.route('/user/<name>')
 def hello(name="vvonder"):
     return template('index', **locals())
+
+
+@app.route('/test')
+def test():
+    s = request.environ.get('beaker.session')
+    s['test'] = s.get('test', 0) + 1
+    s.save()
+    return 'Test conter: %d' % s['test']
 
 
 @app.route('/static/<filename:path>')
 def send_static(filename):
     return static_file(filename, root=STATIC_ROOT)
 
+
+app = SessionMiddleware(app, session_options)
 
 if 'SERVER_SOFTWARE' in os.environ:
     from bae.core.wsgi import WSGIApplication
